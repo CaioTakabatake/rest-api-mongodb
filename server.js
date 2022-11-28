9c6167592283ace1df0c4026f1e9b9079d6f19c0
CaioTakabatake <caio.soldera.taka@gmail.com>
2022-11-27 23:08:56 -0300

    add routes

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const http = require('http');
 
 const port = process.env.PORT || 3000;
+const routes = {
+    '/': 'Rest Api',
+    '/books': 'Books page',
+    '/authors': 'Authors page',
+    '/company': 'Company page',
+};
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Rest Api');
+    res.end(routes[req.url]);
 });
 
 server.listen(port, () => {
